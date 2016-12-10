6f6c49b0660fbb6336ec8616b8ffa9e1eaedefd3
Ko Takagi <knockoutmarch@gmail.com>
2016-12-11 07:55:05 +0900

    classに変更

--- scraper.php
+++ scraper.php
@@ -3,41 +3,58 @@ require_once 'vendor/autoload.php';
 
 use Goutte\Client;
 
-$client = new Client();
+class Scraper {
+    protected $client;
 
-$data = array();
-
-// 棋士ページをGET
-$crawler = $client->request('GET', 'http://www.shogi.or.jp/player/pro/175.html');
-
-// 名前を取得
-$dom = $crawler->filter('div.nameArea');
-$dom->each(function ($node) use (&$data) {
-    $data['name'] = $node->filter('span')->eq(0)->text();
-});
-
-// 画像URLを取得
-$dom = $crawler->filter('div.imgArea img');
-$dom->each(function ($node) use (&$data) {
-    $data['image'] = 'http://www.shogi.or.jp' . $node->attr('src');
-});
-
-// 基本情報を取得
-$dom = $crawler->filter('div.uniqueLayoutElements03 table.tableElements02 tr');
-$dom->each(function ($node) use (&$data) {
-    $th = $node->filter('th')->text();
-    $td = $node->filter('td')->text();
-    if ($th == '棋士番号') {
-        $data['no'] = $td;
-    } else if ($th == '生年月日') { 
-        $data['birthday'] = $td;
-    } else if ($th == '出身地') { 
-        $data['birthplace'] = $td;
-    } else if ($th == '竜王戦') { 
-        $data['ryuou'] = $td;
-    } else if ($th == '順位戦') { 
-        $data['junni'] = $td;
+    public function __construct() {
+        $this->client = new Client();
     }
-});
 
-echo json_encode($data);
+    public function getKishi( $no ) {
+        $data = array();
+
+        // 棋士ページをGET
+        $crawler = $this->client->request('GET', 'http://www.shogi.or.jp/player/pro/' . $no . '.html');
+
+        // 名前を取得
+        $dom = $crawler->filter('div.nameArea');
+        $dom->each(function ($node) use (&$data) {
+            $data['name'] = $node->filter('span')->eq(0)->text();
+        });
+        if (!$data) {
+            // 将棋連盟サイトは404ページが無いので名前の取得で正常判断する
+            return false;
+        }
+
+        // 画像URLを取得
+        $dom = $crawler->filter('div.imgArea img');
+        $dom->each(function ($node) use (&$data) {
+            $data['image'] = 'http://www.shogi.or.jp' . $node->attr('src');
+        });
+
+        // 基本情報を取得
+        $dom = $crawler->filter('div.uniqueLayoutElements03 table.tableElements02 tr');
+        $dom->each(function ($node) use (&$data) {
+            $th = $node->filter('th')->text();
+            $td = $node->filter('td')->text();
+            if ($th == '棋士番号') {
+                $data['no'] = $td;
+            } else if ($th == '生年月日') { 
+                $data['birthday'] = $td;
+            } else if ($th == '出身地') { 
+                $data['birthplace'] = $td;
+            } else if ($th == '竜王戦') { 
+                $data['ryuou'] = $td;
+            } else if ($th == '順位戦') { 
+                $data['junni'] = $td;
+            }
+        });
+
+        return json_encode($data);
+    }
+}
+
+$scraper = new Scraper();
+
+echo $scraper->getKishi(175);
+
