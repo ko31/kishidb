1d93db35fa79597ce194d04eb59c6e8cff8dfd3b
Ko Takagi <knockoutmarch@gmail.com>
2016-12-16 08:57:39 +0900

    OpenStreetMap追加

---
+++ docs/map/get_pos_data.php
@@ -0,0 +1,28 @@
+<?php
+
+$data = file_get_contents("kishi_all.json");
+$data = json_decode($data);
+
+$maps = array();
+
+foreach($data as $kishi) {
+    $result = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($kishi->birthplace).'&key=AIzaSyAtslO_IFvo3f6CEZktmoEuBjhlC67FQ1g');
+    $result = json_decode($result, true);
+    if ($result['status'] != 'OK') {
+        continue;
+    }
+    $maps[] = array(
+        'no' => $kishi->no,
+        'name' => $kishi->name,
+        'birthplace' => $kishi->birthplace,
+        'image' => $kishi->image,
+        'lat' => $result['results'][0]['geometry']['location']['lat'],
+        'lng' => $result['results'][0]['geometry']['location']['lng'],
+    );
+    echo $kishi->no." ".$kishi->name."\n";
+    sleep(1);
+}
+
+$json = json_encode($maps);
+
+file_put_contents('map.json', $json);
